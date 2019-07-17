bb257ddf883e26c6cdf97affd752ce6deb758ab2
kowalus23 <kowalus23@gmail.com>
2019-07-17 16:51:46 +0200

    Updates

---
+++ precache-manifest.6f8686031b8edcb918f36c107c412a86.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12451412efac1baf0f8692f5ca5f8643",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6bedc25f51629729ea41",
+    "url": "/static/css/main.f0420e15.chunk.css"
+  },
+  {
+    "revision": "aefc321b22ab710752bb",
+    "url": "/static/js/2.dbca3f2f.chunk.js"
+  },
+  {
+    "revision": "6bedc25f51629729ea41",
+    "url": "/static/js/main.478cf9f2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
