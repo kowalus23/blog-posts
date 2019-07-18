6bc629eb72d8248026cb10dfa1ecc295be2dc83e
kowalus23 <kowalus23@gmail.com>
2019-07-18 12:55:06 +0200

    updated axios to fetch

--- src/js/actions/index.js
+++ src/js/actions/index.js
@@ -1,41 +1,54 @@
-import posts from '../apis/posts'
 import {CREATE_POST, FETCH_POSTS, FETCH_POST} from "./types";
 
 const API_KEY = '?key=HOUSTONWEGOTAPROBLEM12345';
+const BASE_URL = 'https://reduxblog.herokuapp.com/api';
 
 export const fetchPosts = () => async (dispatch) => {
-  const response = await posts.get(`/posts${API_KEY}`);
-
-  dispatch({
-    type: FETCH_POSTS,
-    payload: response.data
-  });
-
+  const response = await fetch(`${BASE_URL}/posts${API_KEY}`);
+  response.json().then(
+    posts => {
+      dispatch({
+        type: FETCH_POSTS,
+        payload: posts
+      });
+    });
 };
 
 export const createPost = (values) => async (dispatch) => {
-  const response = await posts.post(`/posts${API_KEY}`, values);
-
-  dispatch({
-    type: CREATE_POST,
-    payload: response.data
+  const response = await fetch(`${BASE_URL}/posts${API_KEY}`, {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(values)
+  });
+  response.json().then(post => {
+    dispatch({
+      type: CREATE_POST,
+      payload: post
+    });
   });
 };
 
 export const fetchPost = (id) => async (dispatch) => {
-  const response = await posts.get(`/posts/${id}${API_KEY}`);
-
-  dispatch({
-    type: FETCH_POST,
-    payload: response.data
+  const response = await fetch(`${BASE_URL}/posts/${id}${API_KEY}`);
+  response.json().then(post => {
+    dispatch({
+      type: FETCH_POST,
+      payload: post
+    })
   })
 };
 
 export const deletePost = (id) => async (dispatch) => {
-  const response = await posts.delete(`/posts/${id}${API_KEY}`);
-
-  dispatch({
-    type: FETCH_POST,
-    payload: response.data
+  const response = await fetch(`${BASE_URL}/posts/${id}${API_KEY}`, {
+    method: 'DELETE'
   });
+  response.json().then(post => {
+    dispatch({
+      type: FETCH_POST,
+      payload: post
+    });
+  })
 };
